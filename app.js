082b65069640c9bb28c1f7933911691652e38f4b
Nicolas Flacco <mail.flacco@gmail.com>
2013-02-02 08:50:58 -0800

    demo update content field works

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// Basic idea:
+// Present users with a vote page with two pictures of player, and the results
+// of the previous poll. We use socket.io to handle browser push.
+
 var port = 8000;
 var url = require('url');
 var app = require('http').createServer(handler).listen(8000);
@@ -11,12 +15,11 @@ function handler (req, res) {
   // it is easier and faster to use the basic node.js http server
   // instead of relying on express.
   if (pathname === '/update') {
-    //io.sockets.emit('update', {name: "foo"});
+    io.sockets.emit('update', {name: "foo"});
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Updated!\n');
   } else {
-
-    // Show the index.html file (todo, make a static string to serve?)
+    // Show the index.html file (todo, load on startup only?)
     fs.readFile(__dirname + '/index.html',
     function (err, data) {
       if (err) {
@@ -40,6 +43,7 @@ io.sockets.on('connection', function(socket){
 });
 
 /*
+// This is an interval timer firing every 1000ms
 setInterval(function() {
   io.sockets.emit('news', {test: "xxx!"} );
 }, 1000 );
