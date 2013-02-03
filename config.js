b59377dfeb6e4ac9706d6b3f34a35405d3ce07f0
Nicolas Flacco <mail.flacco@gmail.com>
2013-02-03 09:30:51 -0800

    added db name switch when heroku env vars are detected

--- config.js
+++ config.js
@@ -1,4 +1,10 @@
+var dbName;
+if (process.env.MONGOLAB_URI || process.env.MONGOHQ_URL) // Basically, if we are usinging MongoLab on Heroku
+  dbName = "heroku_app11534851";
+else
+  dbName = "player_info"
+  
 exports.updateInterval = 10000; // ms
 exports.port = process.env.PORT || 8000;
-exports.dbName = "player_info";
+exports.dbName;
 exports.collectionName = "players";
