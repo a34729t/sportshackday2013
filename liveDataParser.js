e68b6e2c46ed0113c8cfac336de53138e27d9a03
Dan Arrington <dan.arrington@gmail.com>
2013-02-02 16:01:01 -0800

    Add test data setter.  Refactor playername out of update.

--- liveDataParser.js
+++ liveDataParser.js
@@ -1,11 +1,21 @@
 var fs = require('fs'),
 xml2js = require('xml2js');
 var http = require('http');
+var test = false;
 
-
+exports.setTest = function(useTest) {
+    test = useTest;
+}
 exports.getLastPlayer = function(callback) {
-    var options = { host: 'api.sportsdatallc.org',
+    if(test) {
+        console.log('set test');
+        var options = { host: 'api.sportsdatallc.org',
+        path: '/nfl-t1/2011/PST/3/NYG/SF/pbp.xml?api_key=yxz2mbqm2xgvfgvpskuybpdn'};
+    } else {
+        console.log('set production');
+        var options = { host: 'api.sportsdatallc.org',
         path: '/nfl-t1/2011/PST/4/NYG/NE/pbp.xml?api_key=yxz2mbqm2xgvfgvpskuybpdn'};
+    }
 
     var request = http.get(options);
     request.on('response', function (response) {
